0d6578279ddbc1759b7fbe09e18c3632f34627e1
Roman <abalokor@mail.ru>
2020-07-08 13:27:33 +0300

    Save messages history to file

--- main_cli.py
+++ main_cli.py
@@ -1,17 +1,26 @@
 import asyncio
+import aiofiles
+from datetime import datetime
 
 
-async def read_chat(host, port):
+
+async def read_chat(host, port, history_file):
     reader, writer = await asyncio.open_connection(host, port)
 
     while True:
+        datetime_now = datetime.now().strftime("%Y.%m.%d %H:%M")
+
         data = await reader.readline()
         if not data:
             break
-        print(data.decode(), end='')
+
+        async with aiofiles.open(history_file, mode="a", encoding="utf-8") as outfile:
+            await outfile.write(f'[{datetime_now}] {data.decode()}')
+            outfile.close()
 
 
 if __name__ == '__main__':
     host = 'minechat.dvmn.org'
     port = 5000
-    asyncio.run(read_chat(host, port))
+    history_file = 'minechat.history'
+    asyncio.run(read_chat(host, port, history_file))
