5b5c02575d062977c0144a1ecd7ee23bfad609a2
Roman <abalokor@mail.ru>
2020-07-08 20:29:11 +0300

    Add check for broken token

--- talk-minechat.py
+++ talk-minechat.py
@@ -2,61 +2,50 @@ import asyncio
 import logging
 import json
 
-
-async def readerData(reader):
-    dataread = await reader.readline()
-    logging.debug(dataread.decode())
-    return dataread.decode()
-
-async def writerData(writer, message):
-    datawrite = message + '\n'
-    writer.write(datawrite.encode())
-    await writer.drain()
-    logging.debug(datawrite)
-
-
-async def write_chat():
+'''
     user = {
         "nickname": "Keen newuser",
         "account_hash": "bd24bee4-c11d-11ea-8c47-0242ac110002"
     }
-    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
-
-    await writerData(writer, input(await readerData(reader)))
-
-    if json.loads(await readerData(reader)) is None:
-        await writerData(writer, input('Неизвестный токен. Проверьте его или зарегистрируйте заново.'))
-
-
-
-
-
-
-
+    
 '''
+
+
+
+async def reader_data(reader):
     dataread = await reader.readline()
-    if dataread:
-        logging.debug(dataread.decode())
+    logging.debug(dataread.decode())
+    return dataread.decode()
 
-    message = input('Введите токен для входа: ')
-
+async def writer_data(writer, message):
     datawrite = message + '\n'
     writer.write(datawrite.encode())
     await writer.drain()
     logging.debug(datawrite)
 
-    dataread = await reader.readline()
-    if dataread:
-        logging.debug(dataread.decode())
-        
-    if json.loads(dataread.decode()) is None:
-        print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-'''
-    # assert json.loads(dataread.decode()) is not None, 'Неизвестный токен. Проверьте его или зарегистрируйте заново.'
+
+async def get_token():
+
+    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
+
+    message = input(await reader_data(reader))
+    await writer_data(writer, message)
+
+    readdata = await reader_data(reader)
+    if message:
+        user = json.loads(readdata)
+        if user is None:
+            print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+        else:
+            print('Вы вошли')
+            return user
+
+    await writer_data(writer, input('Введите новое имя пользователя'))
+    return json.loads(await reader_data(reader))
 
 
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    asyncio.run(write_chat())
+    asyncio.run(get_token())
