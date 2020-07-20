30adac2c2e8d239bf306ab415262428c744a2818
Roman <abalokor@mail.ru>
2020-07-20 21:57:35 +0100

    Refactor code, split by reusable functions.

--- talk-minechat.py
+++ talk-minechat.py
@@ -24,9 +24,8 @@ async def writer_data(writer, message):
     logging.debug(datawrite)
 
 
-async def get_token():
 
-    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
+async def register(reader, writer):
 
     message = input(await reader_data(reader))
     await writer_data(writer, message)
@@ -36,16 +35,47 @@ async def get_token():
         user = json.loads(readdata)
         if user is None:
             print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+            return None
         else:
             print('Вы вошли')
             return user
 
-    await writer_data(writer, input('Введите новое имя пользователя'))
+    await writer_data(writer, input(readdata))
     return json.loads(await reader_data(reader))
 
 
+async def authorise(reader, writer, user):
+    await reader_data(reader)
+    await writer_data(writer, user["account_hash"])
+    readdata = await reader_data(reader)
+
+
+async def submit_message(writer):
+    while True:
+        message = input()
+        await writer_data(writer, message)
+        if message.lower() == 'bye':
+            break
+
+    
+async def minechat():
+    while True:
+        reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
+        user = await register(reader, writer)
+        writer.close()
+        await writer.wait_closed()    
+        if user:
+            break
+
+    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)        
+    await authorise(reader, writer, user)
+    await submit_message(writer)
+
+    writer.close()
+    await writer.wait_closed()    
+        
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    asyncio.run(get_token())
+    asyncio.run(minechat())
